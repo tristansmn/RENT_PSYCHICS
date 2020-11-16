8b8d1419d7970dc239ea06e421848fdeb49b68ff
Tristan Simon <tristan.simon1@outlook.fr>
2020-11-16 18:37:57 +0100

    test

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,25 @@ ActiveRecord::Schema.define(version: 2020_11_16_161628) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "accessories", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.bigint "psychic_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["psychic_id"], name: "index_accessories_on_psychic_id"
+  end
+
+  create_table "psychics", force: :cascade do |t|
+    t.string "specialty"
+    t.string "localisation"
+    t.integer "hour_rate"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_psychics_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -29,4 +48,6 @@ ActiveRecord::Schema.define(version: 2020_11_16_161628) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "accessories", "psychics"
+  add_foreign_key "psychics", "users"
 end
