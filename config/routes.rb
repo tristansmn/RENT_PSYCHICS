4fcbc8889f8ee95118d3ca3d8e364f32dc1a9cfd
Tristan Simon <tristan.simon1@outlook.fr>
2020-11-17 13:19:37 +0100

    change routes for psychics

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :users do
-    resource :psychics 
+    resource :psychics [:create, :new, :show, :index]
   end
 end
