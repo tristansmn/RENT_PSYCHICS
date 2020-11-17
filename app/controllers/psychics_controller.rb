d9344d8ef5e9b9f14e8708aa6d7fcb149c2dcaf1
Tristan Simon <tristan.simon1@outlook.fr>
2020-11-17 13:01:58 +0100

    add controller psychics

--- app/controllers/psychics_controller.rb
+++ app/controllers/psychics_controller.rb
@@ -1,2 +1,35 @@
 class PsychicsController < ApplicationController
+	before_action :find_psychic, only: [ :show ]
+  def index
+  	Psychic.all
+  end
+
+  def new
+  	@psychic = Psychic.new
+  end
+
+  def create
+  	@psychic = Psychic.new(psychic_params)
+  	@psychic.user = current_user
+
+  	if @psychic.save
+  	  redirect_to @psychic, notice: 'Psychic profile was successfuly completed'
+  	else
+  	  render :new
+  	end
+  end
+
+  def show
+  end
+
+
+  private
+  
+  def find_psychic
+  @psychic = Psychic.find(params[:id])
+  end
+
+  def psychic_params
+    params.require(:psychic).permit(:specialty, :localisation, :hour_rate)
+  end
 end
