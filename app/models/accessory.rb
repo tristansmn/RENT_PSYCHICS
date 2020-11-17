17c49d5e3421bd04b25507d379af5f3ecb5eef27
Tristan Simon <tristan.simon1@outlook.fr>
2020-11-17 17:24:32 +0100

    change validations

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -1,6 +1,6 @@
 class Accessory < ApplicationRecord
   belongs_to :psychic, optional: true
-  validates :name, :price, :psychic, presence: true
+  validates :name, :price, presence: true
   validates :price, numericality: { only_integer: true }
   
 
