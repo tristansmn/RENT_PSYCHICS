1ed814d40390ac817597d028980d2dae1c07d2e0
antoine maurel <antoine.maurel@gmail.com>
2020-11-17 16:37:54 +0100

    seed avec les dépendances sur les autres fichiers

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users do
-    resources :psychics, only: [:create, :new]
-  end
+  # resources :users
+  # plus la peine d'avoir le controller user si pas de nesting (car déjà devise)
 
-  resources :psychics, only: [:index, :show] do
+  resources :psychics, only: [:index, :show, :create, :new] do
     resources :bookings, only: [:new, :create]
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_171628) do
+ActiveRecord::Schema.define(version: 2020_11_17_152115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 2020_11_16_171628) do
   create_table "accessories", force: :cascade do |t|
     t.string "name"
     t.integer "price"
-    t.bigint "psychic_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "psychic_id"
     t.index ["psychic_id"], name: "index_accessories_on_psychic_id"
   end
 
