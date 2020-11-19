bd4176f8cbd6c2c2525d0862df576d2aee54867a
Manon Oriol <oriol.manon@gmail.com>
2020-11-20 00:02:05 +0100

    lots of front modifs

--- app/models/psychic.rb
+++ app/models/psychic.rb
@@ -1,4 +1,5 @@
 class Psychic < ApplicationRecord
+  SPEC = %w[blackMagic Voodoo KarmaBooster Visions Ghosts Tarot Divination AstralProjection EnergyMedicine]
   belongs_to :user
   has_many :bookings
   has_many :accessories, dependent: :destroy
