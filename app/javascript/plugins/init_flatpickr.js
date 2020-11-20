8f9aa6b4bc05acbd589badaf7eaf59839823b879
Tristan Simon <tristan.simon1@outlook.fr>
2020-11-20 01:25:42 +0100

    booking form modif + js + accessory

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -4,6 +4,9 @@ const initFlatpickr = () => {
 
   const startDateInput = document.getElementById('booking_start_date');
   const endDateInput = document.getElementById('booking_end_date');
+  const hours = document.querySelector("#hours")
+  const pricePerHour = document.querySelector("#price_per_hour").innerText
+  const price = document.querySelector("#price") 
 
   // Check that the query selector id matches the one you put around your form.
   if (startDateInput) {
@@ -14,12 +17,7 @@ const initFlatpickr = () => {
     dateFormat: "Y-m-d H:i",
   });
 
-  console.log('im in the file')
-
   startDateInput.addEventListener("change", (e) => {
-    if (startDateInput != "") {
-      endDateInput.disabled = false
-    }
     flatpickr(endDateInput, {
       minDate: e.target.value,
       enableTime: true,
@@ -27,6 +25,17 @@ const initFlatpickr = () => {
       });
     })
 };
-}
 
+[startDateInput, endDateInput].forEach(date => {
+  date.addEventListener("change", (event) => {
+  let dateDiff = new Date(endDateInput.value) - new Date(startDateInput.value);
+  let totalHours = Math.ceil(dateDiff / (86400000 / 24));
+  if (startDateInput.value && endDateInput.value) {
+    hours.innerText = totalHours
+    price.innerText = totalHours * pricePerHour
+  }
+});
+})
+}
 export { initFlatpickr };
+
