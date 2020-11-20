a5d182eb99c62eed2b9ce080bb30fb4ce2c39eaa
antoine maurel <antoine.maurel@gmail.com>
2020-11-20 12:34:02 +0100

    changed seed and image display

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@
 # migration à faire car j'ai pas lié de base les accessoires à un psychic
 
 require 'faker'
+require 'uri'
 
 Psychic.destroy_all if Rails.env.development?
 User.destroy_all if Rails.env.development?
@@ -21,11 +22,103 @@ specialties = %w(blackMagic Voodoo KarmaBooster Visions Ghosts Tarot Divination 
 addresses = [
   "20 rue de la pompe Paris",
   "2 avenue des champs elysees Paris",
-  "Kensington Gardens, Kensington, London W8 4PX, Royaume-Uni",
-  "Calle Irlanda, 2, 28019 Madrid, Espagne"
+  "Kensington Gardens London ",
+  "Calle Irlanda 28019 Madrid"
   ]
+puts "création des seeds user et psychic"
+puts "------------------------------------"
 
-20.times do
+antoine = User.create(
+    name: "Antoine",
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "testpassword"
+    # devise sait que "password" est à encrypter, donc pour tester on peut mettre password: (et ce sera "testpassword" le set up)
+    )
+
+manon = User.create(
+    name: "Manon",
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "testpassword"
+    # devise sait que "password" est à encrypter, donc pour tester on peut mettre password: (et ce sera "testpassword" le set up)
+    )
+
+tristan = User.create(
+    name: "Tristan",
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "testpassword"
+    # devise sait que "password" est à encrypter, donc pour tester on peut mettre password: (et ce sera "testpassword" le set up)
+    )
+
+michel = User.create(
+    name: "Michel",
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "testpassword"
+    # devise sait que "password" est à encrypter, donc pour tester on peut mettre password: (et ce sera "testpassword" le set up)
+    )
+
+gege = User.create(
+    name: "Gérard",
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "testpassword"
+    # devise sait que "password" est à encrypter, donc pour tester on peut mettre password: (et ce sera "testpassword" le set up)
+    )
+
+puts "Creating psychic profiles"
+puts "------------------------------------"
+
+file = URI.open('https://res.cloudinary.com/antoine83/image/upload/v1605867886/4alku05tg0no68ce7kqx74rn5ax5.jpg')
+genius = Psychic.new({localisation: addresses.sample,
+      hour_rate: [45 , 50, 55, 60, 70, 80, 2500].sample,
+      specialty: specialties[rand(0..(specialties.length - 1))],
+      user_id: antoine.id})
+genius.user = antoine
+genius.photo.attach(io: file, filename: '#{genius.user.name}.jpg', content_type: 'image/jpg')
+genius.save!
+
+file = URI.open('https://res.cloudinary.com/antoine83/image/upload/v1605870978/psychic_of0o8v.jpg')
+whoopy = Psychic.new({localisation: addresses.sample,
+      hour_rate: [45 , 50, 55, 60, 70, 80, 2500].sample,
+      specialty: specialties[rand(0..(specialties.length - 1))],
+      user_id: manon.id})
+whoopy.user = manon
+whoopy.photo.attach(io: file, filename: '#{whoopy.user.name}.jpg', content_type: 'image/jpg')
+whoopy.save!
+
+file = URI.open('https://res.cloudinary.com/antoine83/image/upload/v1605871005/photo-1600637453426-7c64826b19d9_o7o5nr.jpg')
+wizzard = Psychic.new({localisation: addresses.sample,
+      hour_rate: [45 , 50, 55, 60, 70, 80, 2500].sample,
+      specialty: specialties[rand(0..(specialties.length - 1))],
+      user_id: tristan.id})
+wizzard.user = tristan
+wizzard.photo.attach(io: file, filename: '#{wizzard.user.name}.jpg', content_type: 'image/jpg')
+wizzard.save!
+
+file = URI.open('https://res.cloudinary.com/antoine83/image/upload/v1605870930/photo-1582744709859-2d89c6920cfb_vttgrm.jpg')
+cards = Psychic.new({localisation: addresses.sample,
+      hour_rate: [45 , 50, 55, 60, 70, 80, 2500].sample,
+      specialty: specialties[rand(0..(specialties.length - 1))],
+      user_id: michel.id})
+cards.user = michel
+cards.photo.attach(io: file, filename: '#{cards.user.name}.jpg', content_type: 'image/jpg')
+cards.save!
+
+file = URI.open('https://res.cloudinary.com/antoine83/image/upload/v1605870912/photo-1592215955539-f020e64a7125_niq7zc.jpg')
+zoltar = Psychic.new({localisation: addresses.sample,
+      hour_rate: [45 , 50, 55, 60, 70, 80, 2500].sample,
+      specialty: specialties[rand(0..(specialties.length - 1))],
+      user_id: gege.id})
+zoltar.user = gege
+zoltar.photo.attach(io: file, filename: '#{zoltar.user.name}.jpg', content_type: 'image/jpg')
+zoltar.save!
+
+
+
+6.times do
   User.create(
     name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
@@ -43,20 +136,10 @@ addresses = [
   end
 end
 
+puts "création des seeds accessoires"
+puts "------------------------------------"
+
 accessories.each do |accessory|
   Accessory.create!(name: accessory, price: rand(5..200))
 end
 # il faudrait une table de jointure entre accessories et psychic
-
-# pour booking l erreur pour rails:seed➜  RENT_PSYCHICS git:(master) rails db:seed
-# rails aborted!
-# ArgumentError: You need to supply at least one validation
-# /Users/antoine/code/TRISTAN/RENT_PSYCHICS/app/models/booking.rb:6:in `<class:Booking>'
-# /Users/antoine/code/TRISTAN/RENT_PSYCHICS/app/models/booking.rb:1:in `<main>'
-# /Users/antoine/code/TRISTAN/RENT_PSYCHICS/db/seeds.rb:16:in `<main>'
-# /Users/antoine/code/TRISTAN/RENT_PSYCHICS/bin/rails:9:in `<top (required)>'
-# /Users/antoine/code/TRISTAN/RENT_PSYCHICS/bin/spring:15:in `<top (required)>'
-# ./bin/rails:3:in `load'
-# ./bin/rails:3:in `<main>'
-# Tasks: TOP => db:seed
-# (See full trace by running task with --trace)
